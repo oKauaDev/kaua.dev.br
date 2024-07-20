1c55159ea9011ee8d59c1ee9fc95977b7a3209e4
oKauaDev <kauacomtil021@gmail.com>
2024-07-20 09:42:21 -0300

    fix: fix vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
   integrations: [tailwind()],
+  output: "server",
+  adapter: vercel()
 });
